25698c8c8d5a3dd6964734584d93ab47f020f8b1
DevOcross <153447076+DevOcross@users.noreply.github.com>
2024-04-06 23:47:35 -0700

    3 update

--- navbar.js
+++ navbar.js
@@ -1,23 +1,11 @@
-function addNewTask(){
-    let add_btn = document.getElementById("new-add-btn")
-    let new_task_div = document.getElementById("new-tast-perent-div")
-    let cancel_task_btn = document.getElementById("task-cancel-btn")
-
-     add_btn.addEventListener("click", function(){
-        if (new_task_div.style.display == "none"){
-            new_task_div.style.display = "block"
-        }
-        else{
-            new_task_div.style.display = "none"
-        }
-     })
-
-     cancel_task_btn.addEventListener("click", function(){
-        if (new_task_div.style.display == "block"){
-            new_task_div.style.display = "none"
-        }
-        else{
-            new_task_div.style.display = "block"
-        }
-     })
-}
+function addtask() {
+    var add_btn  = document.getElementById('new-add-btn');
+    var task_div = document.getElementById('new-task-div');
+    if (task_div.style.display == "none") {
+        task_div.style.display = "block";
+      
+    } else {
+      task_div.style.display = "none";
+    }
+  
+  }
