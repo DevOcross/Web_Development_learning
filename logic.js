9a694518f83ff90ee2aa9b20906f565ecced2e8e
DevOcross <153447076+DevOcross@users.noreply.github.com>
2024-04-07 02:02:00 -0700

    6 update

--- logic.js
+++ logic.js
@@ -1,11 +1,18 @@
-function addtask() {
+function addTaskBtn() {
     var add_btn  = document.getElementById('new-add-btn');
     var task_div = document.getElementById('new-task-div');
-    if (task_div.style.display == "none") {
+    var task_List = document.getElementById('tasklist');
+    if (task_div.style.display == "none",
+        task_List.style.display == "block") {
         task_div.style.display = "block";
+        task_List.style.display = "none";
+
       
     } else {
       task_div.style.display = "none";
+      task_List.style.display = "block";
+
     }
   
   }
+
