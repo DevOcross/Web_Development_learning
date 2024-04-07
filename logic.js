da2bfc4600f47c55e39c414cd47080b96bea4660
DevOcross <153447076+DevOcross@users.noreply.github.com>
2024-04-07 18:05:23 +0530

    8 update new Designe

--- logic.js
+++ logic.js
@@ -1,22 +1,9 @@
-function addTaskBtn() {
-    var add_btn  = document.getElementById('new-add-btn');
-    var task_div = document.getElementById('new-task-div');
-    var task_List = document.getElementById('tasklist');
-    var tbd = document.getElementById('task-body-div')
-    if (task_div.style.display == "none",
-        task_List.style.display == "block",
-        tbd.style.display == "block") {
-        task_div.style.display = "block";
-        task_List.style.display = "none";
-        tbd.style.display = "none"
+function newTaskBtn{
+    let nTB = document.getElementById("new_task_btn")
+    let nTBM = document.getElementById("new_task_body_main")
+    let tBI = document.getElementById("task_body_items")
 
-      
-    } else {
-      task_div.style.display = "none";
-      task_List.style.display = "block";
-      tbd.style.display = "block"
-
+    if(){
+        
     }
-  
-  }
-
+}
