68e8a7ef1a57dea4eae18ff517551fc34454ab4f
DevOcross <153447076+DevOcross@users.noreply.github.com>
2024-04-08 09:09:23 -0700

    10 update

--- logic.js
+++ logic.js
@@ -1,5 +1,4 @@
 function newTaskBtn(){
-    let nTB = document.getElementById("new_task_btn")
     let nTBM = document.getElementById("new_task_body_main")
     let tBI = document.getElementById("task_body_items")
 
