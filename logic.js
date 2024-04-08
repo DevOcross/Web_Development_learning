1e90e559fbbf8e0f1e4dbdcd0ec5f64fb77150fa
DevOcross <153447076+DevOcross@users.noreply.github.com>
2024-04-07 19:23:25 -0700

    9 update

--- logic.js
+++ logic.js
@@ -1,9 +1,17 @@
-function newTaskBtn{
+function newTaskBtn(){
     let nTB = document.getElementById("new_task_btn")
     let nTBM = document.getElementById("new_task_body_main")
     let tBI = document.getElementById("task_body_items")
 
-    if(){
-        
+    if(nTBM.style.display == "none",
+       tBI.style.display == "block"){
+        nTBM.style.display = "block"
+        tBI.style.display = "none"
     }
+    else{
+        nTBM.style.display = "none"
+        tBI.style.display = "block"
+    }
+
+    
 }
